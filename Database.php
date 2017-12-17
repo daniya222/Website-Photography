3acd3b71065c2d687b158f7210af20506041e75b
hamimSaudha <31055993+hamimSaudha@users.noreply.github.com>
2017-12-17 17:36:09 +0800

    Create Database.php

---
+++ Database.php
@@ -0,0 +1,73 @@
+<?php
+
+class Database{
+	
+	private $host ="localhost";
+	private $user ="root";
+	private $password ="";
+	private $dbname = "photodb";
+	
+	private $dbh;
+	private $error;
+	private $statement;
+	
+	public function __construct()
+	{
+		//set DSN
+		$dsn = 'mysql:host='.$this->host. ';dbname'.$this->dbname;
+		//set option
+		$option = array(
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_ERRMODE	=> PDO::ERRMODE_EXCEPTION
+		);
+		//create new PDO
+		try{
+			$this->dbh = new PDO($dsn, $this->user, $this->password , $option);
+		}
+		catch(PDOException $e)
+		{
+			 echo 'ERROR!';
+			 print_r( $e );
+		}
+	}
+	public function query($query)
+	{
+		$this->statement = $this->dbh->prepare($query);
+	}
+	public function bind($param, $value, $type = null)
+	{
+		if(is_null($type))
+		{
+			switch(true)
+			{
+				case is_int($value);
+					$type = PDO::PARAM_INT;
+					break;
+				case is_bool($value);
+					$type = PDO::PARAM_BOOL;
+					break;
+				case is_null($value);
+					$type = PDO::PARAM_NULL;
+					break;
+					default;
+					$type = PDO::PARAM_STR;
+			}
+		}
+		$this->statement->bindValue($param, $value, $type);
+		
+	}
+	
+	public function execute()
+	{
+		return $this->statement->execute();
+	}
+	public function lastInsertId()
+	{
+		$this->dbh->lastInsertId();
+	}
+	public function resultSet()
+	{
+		$this->execute();
+		return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+	}
+}
